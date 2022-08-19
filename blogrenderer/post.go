529d879059e7739bf2007d8fb77a307430c25784
Tumypmyp <gaynoullin@gmail.com>
2022-08-19 07:54:08 +0000

    first SanitisedTitle virsion

--- blogrenderer/post.go
+++ blogrenderer/post.go
@@ -1,8 +1,20 @@
 package blogrenderer
 
+import "github.com/gomarkdown/markdown"
+
 type Post struct {
 	Title       string
 	Body        string
 	Description string
 	Tags        []string
+}
+
+type HTMLPost struct {
+	Post
+	HTMLBody string
+}
+
+func (p Post) Render() HTMLPost {
+	md := markdown.ToHTML([]byte(p.Body), nil, nil)
+	return HTMLPost{p, string(md)}
 }
